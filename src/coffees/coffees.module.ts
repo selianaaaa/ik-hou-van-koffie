8d52e3bd4771118172d39a4e244f541b894e4e47
Yulia <yulia@info.nl>
2023-05-24 22:29:39 +0200

    feat: coffee rating added

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -9,5 +9,6 @@ import { Flavor } from "./entities/flavor.entity"
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
